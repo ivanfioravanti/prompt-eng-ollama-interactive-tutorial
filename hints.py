c112a14416f4e5c15efbb8f000ad052b1c95aec3
Ivan Fioravanti <ivan.fioravanti@gmail.com>
2024-12-01 00:11:28 +0100

    Chapter 5 added.

--- hints.py
+++ hints.py
@@ -25,3 +25,14 @@ If you surround "{QUESTION}" in XML tags, how does that change Ollama's response
 exercise_4_3_hint = """The grading function in this exercise is looking for a response that includes the word "brown".
 Try removing one word or section of characters at a time, starting with the parts that make the least sense. Doing this one word at a time will also help you see just how much Ollama can or can't parse and understand."""
 
+exercise_5_1_hint = """The grading function for this exercise is looking for a response that includes the word "Warrior".
+Write more words in Ollama's voice to steer Ollama to act the way you want it to. For instance, instead of "Stephen Curry is the best because," you could write "Stephen Curry is the best and here are three reasons why. 1:"""
+
+exercise_5_2_hint = """The grading function looks for a response of over 5 lines in length that includes the words "cat" and "<haiku>".
+Start simple. Currently, the prompt asks Ollama for one haiku. You can change that and ask for two (or even more). Then if you run into formatting issues, change your prompt to fix that after you've already gotten Ollama to write more than one haiku."""
+
+exercise_5_3_hint = """The grading function in this exercise is looking for a response that contains the words "tail", "cat", and "<haiku>".
+It's helpful to break this exercise down to several steps.								
+1.	Modify the initial prompt template so that Ollama writes two poems.							
+2.	Give Ollama indicators as to what the poems will be about, but instead of writing in the subjects directly (e.g., dog, cat, etc.), replace those subjects with the keywords "{ANIMAL1}" and "{ANIMAL2}".							
+3.	Run the prompt and make sure that the full prompt with variable substitutions has all the words correctly substituted. If not, check to make sure your {bracket} tags are spelled correctly and formatted correctly with single moustache brackets."""
