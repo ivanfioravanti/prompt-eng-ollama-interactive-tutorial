57cd67329365399b8be7379fa82d9649ebbf8386
Ivan Fioravanti <ivan.fioravanti@gmail.com>
2024-11-30 23:04:18 +0100

    Chapter 2 added.

--- hints.py
+++ hints.py
@@ -3,3 +3,12 @@ You can often get Ollama to do what you want simply by asking."""
 
 exercise_1_2_hint = """The grading function in this exercise is looking for answers that contain "Arrr" or "matey".
 There are many ways to solve this, just by asking!"""
+
+exercise_2_1_hint ="""The grading function in this exercise is looking for any answer that includes the word "hola".
+Ask Ollama to reply in Spanish like you would when speaking with a human. It's that simple!"""
+
+exercise_2_2_hint = """The grading function in this exercise is looking for EXACTLY "Michael Jordan".
+How would you ask another human to do this? Reply with no other words? Reply with only the name and nothing else? There are several ways to approach this answer."""
+
+exercise_2_3_hint = """The grading function in this cell is looking for a response that is equal to or greater than 800 words.
+Because LLMs aren't great at counting words yet, you may have to overshoot your target."""
